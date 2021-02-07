85e95f516dcaafac17f22d32a7d201e2ebb07e7f
ShadowFlade <Wolffdrugov@yandex.ru>
2021-02-07 11:19:55 +0300

    restructuring the fs(2)

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // JS
 import './js/'
 // SCSS
-// import './assets/scss/main.scss'
+import './assets/scss/main.scss'
 import './assets/scss/utils/default-input.scss'
 function importAll(resolve) {
   resolve.keys().forEach(resolve)
@@ -9,3 +9,4 @@ function importAll(resolve) {
 
 importAll(require.context('../src/pug/pages', true, /\.js$|\.scss$/))
 importAll(require.context('../src/pug/includes/modules', true, /\.js$|\.scss$/))
+importAll(require.context('../src/pug/layout', true, /\.js$|\.scss$/))

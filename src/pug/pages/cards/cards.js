e629f8a935cd4cf2a64aa0c15f852fd33ec061cb
ShadowFlade <Wolffdrugov@yandex.ru>
2021-09-30 18:43:53 +0300

    h12.fixed roomDetails and sign-in pages

--- src/pug/pages/cards/cards.js
+++ src/pug/pages/cards/cards.js
@@ -12,3 +12,11 @@ import '../../includes/modules/hotel-card/hotel-card.js'
 import '../../includes/modules/toggle/toggle.js'
 import '../../includes/modules/date-picker/date-picker.js'
 import '../../includes/modules/filter-date-dropdown/filter-date-dropdown.js'
+setTimeout(()=>{
+  const datepicker=document.getElementsByClassName('daterangepicker ltr show-calendar opensright moveup')[0]
+  datepicker.style.position='relative'
+  datepicker.style.top='-6px'
+  datepicker.style.left='0px'
+  const target=document.querySelector('.card__datepicker')
+  target.appendChild(datepicker)
+},0)
--- src/pug/pages/roomDetails/roomDetails.js
+++ src/pug/pages/roomDetails/roomDetails.js
@@ -1,11 +1,21 @@
-import './hotel-card.js'
+import '../../includes/modules/hotel-card/hotel-card.js'
 import '../../includes/modules/bullet-list/bullet-list.js'
 import '../../includes/modules/graph/graph.js'
 import '../../includes/modules/comment/comment.js'
 import '../../includes/modules/bill/bill.js'
 import '../../includes/modules/features/features.js'
 import '../../includes/modules/date-picker/date-picker.js'
-import '../../includes/modules/hotel-card/hotel-card.js'
 import '../../includes/modules/dropdownSubmitAndClear/dropdownSubmitAndClear.js'
 import '../../includes/modules/navbar/navbar.js'
 import './roomDetails.scss'
+
+$(function() {
+  $('.hotel-card__slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    fade: false,
+    arrows: true,
+    dots: true,
+  })
+})

3ef1663303c3b07373a8146bb56dacf503899589
ShadowFlade <Wolffdrugov@yandex.ru>
2021-10-01 17:05:31 +0300

    h13.класс должен сущ. в отдельной папке и файле.

--- src/pug/includes/modules/like/like.js
+++ src/pug/includes/modules/like/like.js
@@ -1,32 +1,6 @@
 'use strict'
 import './like.scss'
-class Like {
-  constructor(likeItem,) {
-    this.likeItem=likeItem
-    this.getElemenets()
-    this.bindIncrement()
-  }
-  getElemenets(){
-    this.input=this.likeItem.getElementsByClassName('js-like__input')[0]
-    this.count=this.likeItem.getElementsByClassName('js-like__number')[0]
-    this.countNumber=Number(this.count.textContent)
-  }
-  bindIncrement(){
-    this.clicked=false
-    const bindLike=()=>{
-      if (!this.clicked) {
-        this.countNumber+=1
-        this.clicked=true
-      } else if (this.clicked) {
-        this.countNumber-=1
-        this.clicked=false
-      }
-      this.count.textContent=String(this.countNumber)
-    }
-    this.input.addEventListener('click',bindLike)
-
-  }
-}
+import Like from'../likeClass/likeClass'
 const contentLoaded= () =>{
   const likeItems=Array.from(document.getElementsByClassName('js-like__item'))
   likeItems.forEach(likeItem => {

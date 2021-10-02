f07d2beeb834ab607ac1a006262ee3120ea0a08b
ShadowFlade <Wolffdrugov@yandex.ru>
2021-10-02 14:18:44 +0300

    fixed daterangepicker on landing page

--- src/pug/pages/landingPage/landingPage.js
+++ src/pug/pages/landingPage/landingPage.js
@@ -2,3 +2,6 @@ import '../../includes/modules/rooms-for-you/rooms-for-you.js'
 import '../../includes/modules/navbar/navbar.js'
 import './landingPage.scss'
 import '../../includes/modules/date-picker/date-picker.js'
+import '../../includes/modules/filter-date-dropdown/filter-date-dropdown.js'
+
+

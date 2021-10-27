824fa60ee0b8eabd1c2e93c647a65e4098ceb8e6
ShadowFlade <Wolffdrugov@yandex.ru>
2021-10-27 17:47:35 +0300

    #3 no-mixes (partly)

--- src/pug/components/burger-menu/burger-menu.js
+++ src/pug/components/burger-menu/burger-menu.js
@@ -1,6 +1,6 @@
 import './burger-menu.scss'
 import { BindOutsideClickDetection } from '../../../utils/utils'
 const handleContentLoaded = (e)=>{
-  BindOutsideClickDetection('.js-burger', '.first-ul')
+  BindOutsideClickDetection('.js-burger', '.burger__menu')
 }
 document.addEventListener('DOMContentLoaded', handleContentLoaded)

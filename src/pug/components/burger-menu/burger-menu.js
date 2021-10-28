2758a43b811f820168e3f3aa927fd36513ffe17c
ShadowFlade <Wolffdrugov@yandex.ru>
2021-10-28 20:57:18 +0300

    fixing cards styles

--- src/pug/components/burger-menu/burger-menu.js
+++ src/pug/components/burger-menu/burger-menu.js
@@ -1,6 +1,6 @@
 import './burger-menu.scss'
 import { BindOutsideClickDetection } from '../../../utils/utils'
 const handleContentLoaded = (e)=>{
-  BindOutsideClickDetection('.js-burger', '.burger__menu')
+  BindOutsideClickDetection('.js-burger', '.js-burger__menu')
 }
 document.addEventListener('DOMContentLoaded', handleContentLoaded)

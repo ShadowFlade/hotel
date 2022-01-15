0b72ab144e36985f9715dcb21f442327349097e6
ShadowFlade <Wolffdrugov@yandex.ru>
2022-01-15 23:07:43 +0300

    added airdatepicker

--- src/components/air-datepicker/air-datepicker.js
+++ src/components/air-datepicker/air-datepicker.js
@@ -1,4 +1,10 @@
 import AirDatepicker from 'air-datepicker';
 import 'air-datepicker/air-datepicker.css';
-
-new AirDatepicker('.air-datepicker-item',{range:true})
+import './air-datepicker.scss'
+const options={
+  range:false,
+}
+const handleContentLoaded=()=>{
+  const items=Array.from(document.getElementsByClassName('air-datepicker')).map(item=>new AirDatepicker((item),options))
+}
+document.addEventListener('DOMContentLoaded',handleContentLoaded)

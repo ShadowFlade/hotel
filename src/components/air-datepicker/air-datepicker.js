5cf93265542b53f35d52b82afc2f503881001e52
ShadowFlade <Wolffdrugov@yandex.ru>
2022-01-16 20:42:07 +0300

    #13 calendar is ok, range picking is not

--- src/components/air-datepicker/air-datepicker.js
+++ src/components/air-datepicker/air-datepicker.js
@@ -1,10 +1,36 @@
 import AirDatepicker from 'air-datepicker';
 import 'air-datepicker/air-datepicker.css';
 import './air-datepicker.scss'
+const clearButton={
+  content:'<button class="button button--inline js-button--inline button--undefined js-button--undefined" type="button">Очистить</button>',
+}
+const submitButton={
+  content:'<button class="button button--inline js-button--inline button--undefined js-button--undefined" type="button">Применить</button>'
+}
 const options={
-  range:false,
+  range:true,
+  navTitles:{
+    days:'<i>MMMM</i> yyyy '
+  },
+  buttons:[clearButton,submitButton],
+  visible:false,
+  inline:false,
+}
+
+const toggleDPVisibility=(datepicker)=>{
+
+  if (datepicker.style.display==='block') {
+    datepicker.style.display='none'
+  } else {
+    datepicker.style.display='block'
+  }
 }
 const handleContentLoaded=()=>{
-  const items=Array.from(document.getElementsByClassName('air-datepicker')).map(item=>new AirDatepicker((item),options))
+  Array.from(document.getElementsByClassName('js-date-picker')).map(item=>{
+    const dp=new AirDatepicker((item),options)
+    dp.$datepicker.addEventListener('click',(event)=>event.stopPropagation())
+    item.addEventListener('click',(e)=>{
+      toggleDPVisibility(dp.$datepicker)})
+  })
 }
 document.addEventListener('DOMContentLoaded',handleContentLoaded)
--- src/components/date-picker/date-picker.js
+++ src/components/date-picker/date-picker.js
@@ -2,52 +2,6 @@
 import './date-picker.scss';
 import 'daterangepicker';
 $(function () {
-  const datePicker=  $('.js-date-picker').daterangepicker({
-    // parentEl:$('.js-date-picker'),
-    maxSpan: {
-      days: 60,
-    },
-    opens: 'center',
-    applyButtonClasses: 'hotel-card__applybtn',
-    cancelButtonClasses: 'hotel-card__cancelbtn',
-    locale: {
-      applyLabel: 'применить',
-      cancelLabel: 'очистить',
-      format: 'MM.DD.YYYY',
-      separator: ' - ',
-      fromLabel: 'From',
-      toLabel: 'To',
-      customRangeLabel: 'Custom',
-      weekLabel: 'W',
-      daysOfWeek: ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Суб'],
-      monthNames: [
-        'Январь',
-        'Февраль',
-        'Март',
-        'Апрель',
-        'Май',
-        'Июнь',
-        'Июль',
-        'Август',
-        'Сентябрь',
-        'Октябрь',
-        'Ноябрь',
-        'Декабрь',
-      ],
-      firstDay: 1,
-    },
-    singleDatePicker: true,
-    showCustomRangeLabel: false,
-    startDate: '02/04/2021',
-    endDate: '02/11/2021',
-    autoUpdateInput: true,
-  });
-  console.log(datePicker.data('daterangepicker').element)
-
-  const handleTdClicked = function () {
-    $('.end-date').removeClass('in-range');
-  };
-  $('td').on('click', handleTdClicked);
 
   $('.js-card__datepicker').click();
   $(document).mouseup(function () {

7d4c8b48528eab6879cf0727c359f0b438d83597
ShadowFlade <Wolffdrugov@yandex.ru>
2022-02-04 06:27:34 +0300

    #13 slider,socials and dropdown fixed

--- src/components/range-slider/range-slider.js
+++ src/components/range-slider/range-slider.js
@@ -1,14 +1,31 @@
 import './range-slider.scss';
+const formatTheValue=(number)=>{
+  const results=[]
+  let isOk=true
+  let decrement=3
+  const numberArray=Array.from(number.toString())
+  while(isOk){
+    results.push(numberArray.splice(numberArray.length-decrement,3).join(''))
+    decrement+=3
+    if(numberArray.length<decrement){
+      isOk=false
+      results.push(numberArray.join(''))
+    }
+  }
+  return results.reverse().join(' ')
+}
 $(function () {
   $('.js-range-slider__item').slider({
     range: true,
     step: 500,
-    max: 30000,
-    min: 0,
+    max: 14000,
+    min: 500,
     width: 70,
     slide: function (event, ui) {
-      $('.range-slider__end').text(ui.values[1] + 'P');
-      $('.range-slider__start').text(ui.values[0] + 'P');
+      const value0=formatTheValue(ui.values[0])
+      const value1=formatTheValue(ui.values[1])
+      $('.range-slider__start').text(`${value0}Р ` );
+      $('.range-slider__end').text(` ${value1}Р`);
     },
   });
 });

2bffde7da8d4fc3caf738b41f6b20e52dbd30f40
Vladimir Drugov <wolffdrugov@yandex.ru>
2022-04-19 02:16:32 +0300

    #13 range slider init values fixed

--- src/components/range-slider/range-slider.js
+++ src/components/range-slider/range-slider.js
@@ -1,20 +1,21 @@
 import './range-slider.scss';
-const formatTheValue=(number)=>{
-  const results=[]
-  let isOk=true
-  let decrement=3
-  const numberArray=Array.from(number.toString())
-  while(isOk){
-    results.push(numberArray.splice(numberArray.length-decrement,3).join(''))
-    decrement+=3
-    if(numberArray.length<decrement){
-      isOk=false
-      results.push(numberArray.join(''))
+const formatTheValue = (number) => {
+  const results = [];
+  let isOk = true;
+  let decrement = 3;
+  const numberArray = Array.from(number.toString());
+  while (isOk) {
+    results.push(numberArray.splice(numberArray.length - decrement, 3).join(''));
+    decrement += 3;
+    if (numberArray.length < decrement) {
+      isOk = false;
+      results.push(numberArray.join(''));
     }
   }
-  return results.reverse().join(' ')
-}
+  return results.reverse().join(' ');
+};
 $(function () {
+  const sliderValues = [500, 7000];
   $('.js-range-slider__item').slider({
     range: true,
     step: 500,
@@ -22,10 +23,15 @@ $(function () {
     min: 500,
     width: 70,
     slide: function (event, ui) {
-      const value0=formatTheValue(ui.values[0])
-      const value1=formatTheValue(ui.values[1])
-      $('.range-slider__start').text(`${value0}Р ` );
+      const value0 = formatTheValue(ui.values[0]);
+      const value1 = formatTheValue(ui.values[1]);
+      $('.range-slider__start').text(`${value0}Р `);
       $('.range-slider__end').text(` ${value1}Р`);
     },
+    values: sliderValues,
   });
+
+  Array.from(document.getElementsByClassName('range-slider__start'))[0].textContent =
+    sliderValues[0];
+  Array.from(document.getElementsByClassName('range-slider__end'))[0].textContent = sliderValues[1];
 });

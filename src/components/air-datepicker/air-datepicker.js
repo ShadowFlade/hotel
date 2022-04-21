af40007048b94ff632f4e851b5bbb304df7a9ede
Vladimir Drugov <wolffdrugov@yandex.ru>
2022-04-21 04:58:30 +0300

    #13 placeholder in calendar now picks startDate

--- src/components/air-datepicker/air-datepicker.js
+++ src/components/air-datepicker/air-datepicker.js
@@ -1,12 +1,15 @@
+// prettier-ignore
 import AirDatepicker from 'air-datepicker';
 import 'air-datepicker/air-datepicker.css';
 import { bindOutsideClickDetection, getDOMElement } from '../../utils/utils';
 import './air-datepicker.scss';
 const clearButton = {
-  content: '<button class="button button--inline js-button--inline" type="button">Очистить</button>',
+  content:
+    '<button class="button button--inline js-button--inline" type="button">Очистить</button>',
 };
 const submitButton = {
-  content: '<button class="button button--inline js-button--inline" type="button">Применить</button>',
+  content:
+    '<button class="button button--inline js-button--inline" type="button">Применить</button>',
 };
 const options = {
   moveToOtherMonthsOnSelect: false,
@@ -35,17 +38,44 @@ const hideDP = (datepicker) => {
     return false;
   }
 };
+const readDatePlaceholder = (elements) => {
+  const pattern = new RegExp(/(\d{2})\.(\d{2})\.(\d+)/);
+  const replace = new RegExp(/\./, 'gi');
+  const placeholders = [];
+  elements.forEach((element) => {
+    const placeholder = Array.from(element.getElementsByTagName('input')).at(0).placeholder;
+    const match = placeholder.match(pattern);
+    if (match) {
+      const formattedDate = match
+        .slice(1)
+        .reverse()
+        .map((item) => item)
+        .join('-');
+      placeholders.push(formattedDate);
+    }
+  });
+  return placeholders.at(-1)?.trim();
+};
 
-const bindCalendar = ({ inputsClassname, optionsForCalendar = options, parentElementClassname }) => {
-  const parentElement = Array.from(document.getElementsByClassName(parentElementClassname))[0] || document;
+const bindCalendar = ({
+  inputsClassname,
+  optionsForCalendar = options,
+  parentElementClassname,
+}) => {
+  const parentElement =
+    Array.from(document.getElementsByClassName(parentElementClassname))[0] || document;
   const inputElements = Array.from(parentElement.getElementsByClassName(inputsClassname));
-  const dp = new AirDatepicker(inputElements.at(0), optionsForCalendar);
+  const placeholder = readDatePlaceholder(inputElements);
+  const dp = new AirDatepicker(inputElements.at(0), {
+    ...optionsForCalendar,
+    startDate: placeholder,
+  });
+  dp.selectDate(placeholder);
   Array.from(document.getElementsByClassName('js-button--inline')).forEach((item) =>
     item.addEventListener('click', (e) => {
       e.preventDefault();
     })
   );
-  console.log(inputElements.at(0), 'needed element');
   inputElements.forEach((input) => {
     if (!!parentElementClassname && !!parentElement && !parentElement.contains(input)) {
       console.warn('No parent element found');
